48cee5b965ef6c124dcc1985c53efadc7e4c5b3f
Yan Sun <tsun812@gmail.com>
2022-03-03 14:16:05 -0800

    bracket validator using Stack and array's includes for comparison

--- bracketChecker.js
+++ bracketChecker.js
@@ -25,5 +25,27 @@ const bracketChecker = function (String1) {
 };
 console.log(bracketChecker("{{}"));
 
-
-//stack, array's push and pop methods. Use Set's has method instead of multiple Or
+//stack, array's push and pop methods. Use array's includes instead of multiple or
+const bracketCheckerStack = function (String1) {
+  let openArr = ["[", "{", "("];
+  let closeArr = ["]", "}", ")"];
+  let currOpenStack = [];
+  if (String1.length === 0 || String1.length === 1) return false;
+  for (let i = 0; i < String1.length; i++) {
+    console.log(String1[i]);
+    if (openArr.includes(String1[i])) {
+      currOpenStack.push(String1[i]);
+    } else if (closeArr.includes(String1[i])) {
+      console.log("hello")
+      if (!currOpenStack) return false;
+      let currOpen = currOpenStack.pop();
+      if (openArr.indexOf(currOpen) !== closeArr.indexOf(String1[i])) {
+        return false;
+      }
+    } else return false;
+  }
+  if (currOpenStack.length === 0) {
+    return true;
+  } else return false;
+};
+console.log(bracketCheckerStack("{()}"));
