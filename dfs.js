130816e82c0ce1c47c75fe94386f57a4cc1a3bd5
Yan Sun <tsun812@gmail.com>
2022-03-07 20:28:18 -0800

    dfs recursive

---
+++ dfs.js
@@ -0,0 +1,25 @@
+let { TreeNode } = require("./tree");
+//O(n) time and space
+
+function dfs(node, value) {
+  if(node.value === value) return node;
+  if (node.getChildren()) {
+    console.log(node.value)
+    for (var i = 0; i < node.getChildren().length; i++) {
+      dfs(node.getChildren()[i], value);
+    }
+  
+  }
+  return null; 
+}
+
+let root = new TreeNode("root");
+let child1 = new TreeNode("1");
+let child2 = new TreeNode("2");
+let child3 = new TreeNode("3");
+let child4 = new TreeNode("4");
+root.addChild(child1);
+root.addChild(child2);
+child1.addChild(child3);
+child2.addChild(child4);
+dfs(root, "3");
